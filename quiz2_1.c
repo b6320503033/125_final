7f3bb69fe6df12f4527baea54bccdaae50f0e9b1
b6320503033 <thida.sa@ku.th>
2021-03-29 18:29:39 +0700

    Update quiz2_1.c

--- quiz2_1.c
+++ quiz2_1.c
@@ -6,33 +6,37 @@ int main()
     int k[5];
     scanf("%d %d",&n,&c);
 
-    tn=n;
-    while(tn>0)
+    if(n>=0&&n<=32750)
     {
-        k[i]=tn%10;
-        tn/=10;
-        i++;
-        y*=10;
-    }
-    y/=10;
-    if(k[0]>c)
-    {
-        k[0]=c;
-        k[1]++;
-        for(i--; i>=0; i--,y/=10)
+
+        tn=n;
+        while(tn>0)
         {
-            x=x+(k[i]*y);
+            k[i]=tn%10;
+            tn/=10;
+            i++;
+            y*=10;
         }
-    }
-    else
-    {
-        k[0]=c;
-        for(i--; i>=0; i--,y/=10)
+        y/=10;
+        if(k[0]>c)
         {
-            x=x+(k[i]*y);
+            k[0]=c;
+            k[1]++;
+            for(i--; i>=0; i--,y/=10)
+            {
+                x=x+(k[i]*y);
+            }
         }
+        else
+        {
+            k[0]=c;
+            for(i--; i>=0; i--,y/=10)
+            {
+                x=x+(k[i]*y);
+            }
+        }
+        printf("%d",x);
     }
-    printf("%d",x);
 
 
     return 0;
